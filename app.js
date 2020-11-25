1f2ff8f132d06c6eb3e35c65126cb15ce661a2e9
MelihErpek <61746050+MelihErpek@users.noreply.github.com>
2020-11-25 17:34:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+const nodemailer = require("nodemailer");
+
+app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use("/assets",express.static("assets"))
+app.get("/",(req,res)=>{
+    res.render('index')})
+
+app.get("/about",(req,res)=>{
+    //res.send("about sayfasındasınız.")
+    res.render('about');
+})
+app.get("/iletisim",(req,res)=>{
+    res.render('iletisim')
+})
+app.post("/iletisim",(req,res)=>{
+    var transfer = nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:"melihnode@gmail.com",
+            pass:"meliherpek1"
+        }
+    });
+
+    var mailInfo ={
+        from:"melih.erpek1@ogr.sakarya.edu.tr",
+        to:req.body.mail,
+        subject:"EJS,NodesJS ile atılan mail",
+        text:req.body.mesaj
+    };
+
+    transfer.sendMail(mailInfo,function(err){
+        if (err) {console.log(err);}
+        else console.log("gönderildi")    
+            
+        });
+
+
+    
+    res.end();
+})
+app.get("/profile/:name",(req,res)=>{
+    //res.send(`${req.params.name} adlı kişinin profilindesin.`)
+    //res.sendFile(__dirname+"/profile.html");
+let personelInfo = {age : 30 , job : 'developer',langs : ['İngilizce','İtalyanca', 'Almanca,'],admin : true};
+
+    res.render('profile',{kisi : req.params.name ,data : personelInfo});
+})
+
+app.listen(3000)    
